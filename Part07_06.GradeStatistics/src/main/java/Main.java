96dcbafaef52c7b2ba51b058b1cfaa81c3b4bc10
Tristán Lacruz <lacruztristan@gmail.com>
2024-07-01 12:00:52 +0200

    Exercise Part07-06-GradeStatistics

--- Part07_06.GradeStatistics/src/main/java/Main.java
+++ Part07_06.GradeStatistics/src/main/java/Main.java
@@ -1,12 +1,13 @@
-
 import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
+
         Scanner scanner = new Scanner(System.in);
-        
-        // Write your program here -- consider breaking the program into 
-        // multiple classes.
+        GradeRegister register = new GradeRegister();
+
+        UserInterface userInterface = new UserInterface(scanner, register);
+        userInterface.start();
     }
 }
